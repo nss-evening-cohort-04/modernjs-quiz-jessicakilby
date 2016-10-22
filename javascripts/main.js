43c9d63c2827905cecf83236f5e7862407274b28
jessica kilby <jessicakilby@gmail.com>
2016-10-22 10:11:02 -0500

    select option and main robot added, outputDiv linked to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,28 @@
 "use strict";
 
-console.log("hey");
+const Robot = () => {
+	this.baseDamage = Math.floor(Math.random() * 10);
+	this.health = 500;
+	this.weapon = null;
+}
+Robot.prototype.attack = function (target) {
+  target.health -= this.baseDamage;
+}
+
+
+const outputDiv = $("#output");
+let output = '<p>outputDiv</p>';
+outputDiv.append(output);
+//Drone, Bipedal, ATV
+
+// var sum = function () {
+// 	let a=1, b=2;
+// 	return a+b;
+// }
+// console.log("hardcoded sum in es5", sum());
+
+// var sum2 = () => {
+// 	let a=1, b=2;
+// 	return a+b;
+// }
+// console.log("hardcoded sum2 in es5", sum2());
